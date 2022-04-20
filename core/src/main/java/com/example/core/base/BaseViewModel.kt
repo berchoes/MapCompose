fd8662134d8ab62a2aae9b50e78ce5723e6b7f0a
Berk Çelik <berk.celik@appcent.mobi>
2022-04-20 13:16:19 +0300

    initial commit

---
+++ core/src/main/java/com/example/core/base/BaseViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.core.base
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+/**
+ * Created by Berk Ç. on 20.04.2022.
+ */
+
+@HiltViewModel
+class BaseViewModel: ViewModel() {
+}
---
+++ core/src/main/java/com/example/core/di/CoreModule.kt
@@ -0,0 +1,45 @@
+package com.example.core.di
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+/**
+ * Created by Berk Ç. on 19.04.2022.
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object CoreModule {
+
+    @Provides
+    @Singleton
+    internal fun provideGson(): Gson = GsonBuilder().create()
+
+
+    @Singleton
+    @Provides
+    internal fun provideOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BASIC
+        }
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+
+    @Singleton
+    @Provides
+    internal fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl("BASE_URL")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+}
